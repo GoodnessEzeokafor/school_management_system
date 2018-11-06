30ed402bf43c9d7955b89925447c5e707f5959ff
Ezeokafor <gootech442@gmail.com>
2018-11-07 00:19:17 +0100

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '3-suu@bmeyw=w&(vmj5%i=*pkhoyf!0#x955tx4a+m*zwajtfd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'test-app-proto.herokuapp.com']
 
 
 
