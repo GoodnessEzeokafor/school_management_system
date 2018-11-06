79822d0ffe9a48788497635511eb979838983bf5
Ezeokafor <gootech442@gmail.com>
2018-11-07 00:23:47 +0100

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3-suu@bmeyw=w&(vmj5%i=*pkhoyf!0#x955tx4a+m*zwajtfd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG =True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'test-app-proto.herokuapp.com']
 
